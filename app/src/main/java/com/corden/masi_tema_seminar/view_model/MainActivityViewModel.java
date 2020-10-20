277770abc16117edbe89945e629acbaee30c0696
Cristian VOICU <cristyan.voicu@gmail.com>
2020-10-20 16:21:04 +0300

    Added Jacobi and Legendre classes.

---
+++ app/src/main/java/com/corden/masi_tema_seminar/view_model/MainActivityViewModel.java
@@ -0,0 +1,12 @@
+package com.corden.masi_tema_seminar.view_model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
