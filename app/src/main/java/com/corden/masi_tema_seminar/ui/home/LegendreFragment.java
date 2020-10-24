8f70e9c44ac6fb614a25b05ba751513d0da667fc
Cristian VOICU <cristyan.voicu@gmail.com>
2020-10-24 17:35:06 +0300

    Added UI for legendre.

--- app/src/main/java/com/corden/masi_tema_seminar/ui/home/LegendreFragment.java
+++ app/src/main/java/com/corden/masi_tema_seminar/ui/home/LegendreFragment.java
@@ -7,27 +7,33 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import com.corden.masi_tema_seminar.R;
+import com.corden.masi_tema_seminar.databinding.FragmentJacobiBinding;
+import com.corden.masi_tema_seminar.databinding.FragmentLegendreBinding;
+import com.corden.masi_tema_seminar.ui.jacobi.JacobiViewModel;
 
 public class LegendreFragment extends Fragment {
-
-    private LegendreViewModel legendreViewModel;
+    private FragmentLegendreBinding mBiding;
+    private LegendreViewModel mViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        legendreViewModel =
-                ViewModelProviders.of(this).get(LegendreViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_legendre, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        legendreViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        mViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(this.getActivity().getApplication()).create(LegendreViewModel.class);
+        mBiding = DataBindingUtil.inflate(inflater, R.layout.fragment_legendre, container, false);
+        mBiding.setViewModel(mViewModel);
+
+        mViewModel.result.observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                mBiding.result.setText(s);
             }
         });
-        return root;
+
+        return mBiding.getRoot();
     }
 }
--- app/src/main/java/com/corden/masi_tema_seminar/ui/home/LegendreViewModel.java
+++ app/src/main/java/com/corden/masi_tema_seminar/ui/home/LegendreViewModel.java
@@ -4,16 +4,32 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.corden.masi_tema_seminar.exception.JacobiException;
+import com.corden.masi_tema_seminar.exception.LegendreException;
+import com.corden.masi_tema_seminar.model.Jacobi;
+import com.corden.masi_tema_seminar.model.Legendre;
+
 public class LegendreViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    public MutableLiveData<String> a = new MutableLiveData<>();
+    public MutableLiveData<String> p = new MutableLiveData<>();
 
-    public LegendreViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Legendre fragment");
+    public MutableLiveData<String> result = new MutableLiveData<>();
+
+    public void onCalculate(){
+        try {
+            int p = Integer.parseInt(this.p.getValue());
+            int a = Integer.parseInt(this.a.getValue());
+            Legendre legendre = new Legendre(a, p);
+            result.setValue(Integer.toString(legendre.calculateLegendreSymbol()));
+        } catch (NumberFormatException e) {
+            result.setValue("You must insert only numbers!");
+        } catch (NullPointerException e){
+            result.setValue("You must enter values!");
+        } catch (LegendreException e) {
+            result.setValue("Number does not meet legendre criteria!");
+        }
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+
 }
